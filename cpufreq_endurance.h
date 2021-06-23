b9d8fbfa89d5232b4184bb678bef14d21d733a29
Tejas Udupa <tejasudupa1285@gmail.com>
2021-06-23 20:34:36 +0530

    Remove atoi as the code has been reworked to use kernel built in functions
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

--- cpufreq_endurance.h
+++ cpufreq_endurance.h
@@ -1,29 +1,4 @@
 #ifndef _CPUFREQ_PERFPLUS_H_
 #define _CPUFREQ_PERFPLUS_H_
 
-#include <linux/string.h>
-#include <linux/ctype.h>
-/*
- *  ======== atoi ========
- *  Purpose:
- *      This function converts strings in decimal or hex format to integers.
- */
-static s32 atoi(char *psz_buf)
-{
-	char *pch = psz_buf;
-	s32 base = 0;
-
-	while (isspace(*pch))
-		pch++;
-
-	if (*pch == '-' || *pch == '+') {
-		base = 10;
-		pch++;
-	} else if (*pch && tolower(pch[strlen(pch) - 1]) == 'h') {
-		base = 16;
-	}
-
-	return simple_strtoul(pch, NULL, base);
-}
-
 #endif
