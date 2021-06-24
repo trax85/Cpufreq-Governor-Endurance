6f1ecbd011f3fefa7dee76c6f6aa79aa6c3be378
Tejas Udupa <tejasudupa1285@gmail.com>
2021-06-24 16:03:25 +0530

    Replace printk with PDEBUG
    - Now we have debug sections that can be toggled on and off before 
    building kernel
    - Makes code look lot more cleaner and better output log formatting
    
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

--- cpufreq_endurance.c
+++ cpufreq_endurance.c
@@ -89,7 +89,7 @@ int get_cpufreq_table(struct cpufreq_policy *policy){
 	if(cluster)
 		goto setup_done;
 	
-	pr_debug(KERN_INFO"%s: starting frequency table init of core:%d\n", __func__,policy->cpu);
+	PDEBUG("%s: starting frequency table init of core:%d", __func__,policy->cpu);
 	
 	/* Start cpu frequency table initialization */
 	ret = init_cpufreq_table(policy);
@@ -110,20 +110,22 @@ int get_cpufreq_table(struct cpufreq_policy *policy){
 		temp = cluster->cur_temps - cluster->throt_temps;
 		if(temp)
 			temp = temp / 2;
-		printk(KERN_INFO"go down levels by:%d\n",temp);
+		PDEBUG("go down levels by:%d",temp);
 		for(i = 0; i <= temp; i++)
 			do_cpufreq_mitigation(policy, cluster, THROTTLE_DOWN);
 	}
 	
 	// Debugging functions
-	for(i = 0; i <= cluster->nr_levels; i++)
-		printk(KERN_INFO"%u ",cluster->freq_table[i].frequency);
-	printk(KERN_INFO"\n");
+	if(debug){
+		for(i = 0; i <= cluster->nr_levels; i++)
+			PDEBUG("%u ",cluster->freq_table[i].frequency);
+		PDEBUG("\n");
+	}
 
 setup_done:
 	cluster->max_freq = cluster->prev_freq = policy->max;
 	cluster->governor_enabled = true;
-	printk(KERN_INFO"governor state:%d",cluster->governor_enabled);
+	PDEBUG("governor state:%d",cluster->governor_enabled);
 	return 0;
 
 failed_inittbl:
@@ -152,7 +154,7 @@ int init_cpufreq_table(struct cpufreq_policy *policy)
 	freq_table = cpufreq_frequency_get_table(policy->cpu);
 	cpufreq_frequency_table_target(policy,freq_table,freqmax,
 				CPUFREQ_RELATION_H,&index);
-	printk(KERN_INFO"Index:%d\n",index);
+	PDEBUG("Index:%d",index);
 
 	/* Initialise the cluster_prop structure. */
 	if(!cluster){
@@ -189,7 +191,6 @@ int get_sensor_dat(struct cluster_prop *cluster)
 	if((cluster->cur_temps > 100) || (cluster->cur_temps < 0))
 		goto fail;
 
-	//printk(KERN_INFO"%s: sensor:%d data initilaised.\tcur_temps: %ld\n",__func__, SENSOR_ID, cluster->cur_temps);
 	return 0;
 fail:
 	pr_err(KERN_WARNING"%s: Failed to get sensor: %d temprature data.\n", __func__, SENSOR_ID);
@@ -252,14 +253,14 @@ static int govern_cpu(struct cluster_prop *cluster)
 
 	/* Update current frequency if changed explicitly by user or other programs */
 	if(cluster->max_freq != policy->max){
-			printk("Prev max freq:%u New max freq:%u\n",cluster->max_freq,policy->max);
+			PDEBUG("Prev max freq:%u New max freq:%u",cluster->max_freq,policy->max);
 			do_cpufreq_mitigation(policy, cluster, UPDATE);
 	}
 		
 	/* either we have not yet reached our cluster throttle temps or we dropped below throttle temps, 
 	   so reset cluster levels and push max frequency of that cluster */
 	if(cluster->cur_temps < cluster->throt_temps){
-		printk("Currrent temps lower than throttle temps\n");
+		PDEBUG("Currrent temps lower than throttle temps");
 		if((cluster->prev_temps < cluster->throt_temps) && (cluster->cur_level == cluster->nr_levels))
 			goto end;
 		else
@@ -272,21 +273,21 @@ static int govern_cpu(struct cluster_prop *cluster)
 	   3.temps have started drop to downward slope 
 	   4-5.temps reported are inconsistent */
 	else if(cluster->cur_temps >= cluster->throt_temps){
-		printk("Current temps higher than throttle temps\n");
+		PDEBUG("Current temps higher than throttle temps");
 		if((cluster->cur_temps == cluster->throt_temps) && (cluster->cur_temps > cluster->prev_temps))
 			do_cpufreq_mitigation(policy, cluster, THROTTLE_DOWN);
 	
 		/* temps have gone past throttle points and now frequency of respective cluster is being mitigated down 
 	   	   through multiple levels depending on how much the temps have gone up since last recorded. */
 		else if(cluster->cur_temps == (cluster->prev_temps + temp_diff)){
-			printk("current temps higher than previous\n");
+			PDEBUG("current temps higher than previous");
 			do_cpufreq_mitigation(policy, cluster, THROTTLE_DOWN);
 		}
 		
 		/* temps have started to drop either due to low avg load or idleing of the cluster,
 	   	   so start throttling the core up by one level as the temps drop. */
 		else if((cluster->cur_temps + temp_diff) == cluster->prev_temps){
-			printk("current temps lower than previous\n");
+			PDEBUG("current temps lower than previous");
 			do_cpufreq_mitigation(policy, cluster, THROTTLE_UP);
 		}
 			
@@ -296,14 +297,14 @@ static int govern_cpu(struct cluster_prop *cluster)
 
 		else if(cluster->cur_temps > (cluster->prev_temps + temp_diff)){
 			temp = (cluster->cur_temps - cluster->prev_temps) / 2;
-			printk("current temps dont match previous DOWN max_dif:%d\n",temp);
+			PDEBUG("current temps dont match previous DOWN max_dif:%d",temp);
 			for(i = 0; i<=temp ;i++)
 				do_cpufreq_mitigation(policy, cluster, THROTTLE_DOWN);
 		}
 		
 		else if(cluster->prev_temps > (cluster->cur_temps + temp_diff)){
 			temp = (cluster->prev_temps - cluster->cur_temps) / 2;
-			printk("current temps dont match previous UP max_dif:%d\n",temp);
+			PDEBUG("current temps dont match previous UP max_dif:%d",temp);
 			for(i = 0; i<=temp; i++)
 				do_cpufreq_mitigation(policy, cluster, THROTTLE_UP);
 		}		
@@ -313,7 +314,7 @@ static int govern_cpu(struct cluster_prop *cluster)
 	return 0;
 
 end:
-	pr_debug("no cpufreq changes required\n");
+	PDEBUG("no cpufreq changes required");
 	return 0;
 
 failed:
@@ -332,36 +333,36 @@ int do_cpufreq_mitigation(struct cpufreq_policy *policy,
 	switch(event){
 		case RESET:	
 			cluster->cur_level = cluster->nr_levels;
-			printk("RESET\n");
+			PDEBUG("RESET");
 			break;
 		case NO_CHANGE:
-			printk("NO_CHANGE\n");
+			PDEBUG("NO_CHANGE");
 			goto end;
 		case THROTTLE_DOWN:
 			if((cluster->cur_level > (cluster->cur_level - min_step)) && (cluster->cur_level != 0)){
 			cluster->cur_level--;
-			printk("THROTTLE_DOWN\n");
+			PDEBUG("THROTTLE_DOWN");
 			}else
-				printk("MAX_THROTTLE\n");
+				printk("MAX_THROTTLE");
 			break;
 		case THROTTLE_UP:
 			if(cluster->cur_level < cluster->nr_levels){
 				cluster->cur_level++;
-				printk("THROTTLE_UP\n");
+				PDEBUG("THROTTLE_UP");
 			}break;
 		case UPDATE:
-			printk("UPDATE\n");
+			PDEBUG("UPDATE");
 			goto update;
 		default:
 			break;
 	}
-	printk(KERN_INFO"cur_temps:%ld throt_temps:%d prev_temps:%ld\n",cluster->cur_temps,cluster->throt_temps,
+	PDEBUG("cur_temps:%ld throt_temps:%d prev_temps:%ld",cluster->cur_temps,cluster->throt_temps,
 					cluster->prev_temps);
 	cluster->prev_temps = cluster->cur_temps;
 
 update:
 	cluster->max_freq = policy->max;
-	printk(KERN_INFO"THROTTLE to %u from %u level:%d max_lvl:%d cpu:%d\n",cluster->freq_table[cluster->cur_level].frequency,
+	PDEBUG("THROTTLE to %u from %u level:%d max_lvl:%d cpu:%d",cluster->freq_table[cluster->cur_level].frequency,
 			policy->max,cluster->cur_level,cluster->nr_levels, policy->cpu);
 	policy->max = cluster->freq_table[cluster->cur_level].frequency;
 	cluster->prev_freq = policy->max;
@@ -384,7 +385,7 @@ static int cpufreq_endurance_speedchange_task(void *data){
 	struct cluster_prop *cluster;
 	kthread_awake = true;
 	
-	printk(KERN_INFO"CFEndurance Running wake:%d\n",kthread_awake);
+	PDEBUG("CFEndurance Running wake:%d",kthread_awake);
 	while (!kthread_should_stop()) {
 		set_current_state(TASK_RUNNING);
 
@@ -412,7 +413,7 @@ static int cpufreq_endurance_speedchange_task(void *data){
 	return 0;
 done:
 	kthread_awake = false;
-	printk(KERN_INFO"Offlining Endurance Governor wake:%d\n",kthread_awake);
+	PDEBUG("Offlining Endurance Governor wake:%d",kthread_awake);
 	return 0;
 	
 }
@@ -428,7 +429,7 @@ int start_gov_setup(struct cpufreq_policy *policy)
 	struct sched_param param = { .sched_priority = MAX_RT_PRIO-1 };
 	int err = 0;
 	
-	printk(KERN_INFO"Starting Governor for cpu:%d\n",policy->cpu);
+	PDEBUG("Starting Governor for cpu:%d",policy->cpu);
 	
 	/* aquire lock so that we dont overwrite critical sections which could lead to wrong 
 		   data being taken in and improper working of governor */
@@ -438,7 +439,7 @@ int start_gov_setup(struct cpufreq_policy *policy)
 		goto error;
 	
 	mutex_unlock(&gov_lock);
-	printk(KERN_INFO"Finished setup wake:%d\n",kthread_awake);
+	PDEBUG("Finished setup wake:%d",kthread_awake);
 	
 	/* setup kthread for endurance governing skip is it has already been setup */
 	if(kthread_awake == false){
@@ -452,7 +453,7 @@ int start_gov_setup(struct cpufreq_policy *policy)
 		get_task_struct(speedchange_task);
 		/* NB: wake up so the thread does not look hung to the freezer */
 		wake_up_process(speedchange_task);
-		printk(KERN_INFO"Run kthread\n");
+		PDEBUG("Run kthread");
 	}
 	
 	return 0;
--- cpufreq_endurance.h
+++ cpufreq_endurance.h
@@ -1,4 +1,13 @@
-#ifndef _CPUFREQ_PERFPLUS_H_
-#define _CPUFREQ_PERFPLUS_H_
+#ifndef _CPU_FREQ_ENDURANCE_H_
+#define _CPU_FREQ_ENDURANCE_H_
+
+static unsigned int debug = 1;
+
+#define PDEBUG(fmt, args...)	 {					\
+	if(debug != 0){						\
+		printk( KERN_INFO "CFE: " fmt, ##args);	\
+		printk("\n");						\
+	}								\
+}
 
 #endif
